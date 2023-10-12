aa190efc362f655077de8894126355c4d05ff85c
Elisa <3bperezelisa@gmail.com>
2023-10-12 09:33:39 +0200

    modificar exposicion mejorado

--- ApiRestConcesionario/src/main/java/com/ApiRestConcesionario/Controller/ExposicionOutputNombre.java
+++ ApiRestConcesionario/src/main/java/com/ApiRestConcesionario/Controller/ExposicionOutputNombre.java
@@ -1,14 +1,18 @@
 package com.ApiRestConcesionario.Controller;
 
+import com.ApiRestConcesionario.Exception.IsEmptyException;
+import com.ApiRestConcesionario.Exception.NullException;
+
 public class ExposicionOutputNombre {
 
     private String nombre;
 
-    public ExposicionOutputNombre(String nombre) {
+    public ExposicionOutputNombre(String nombre){
         this.nombre = nombre;
+
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public String getNombre() {
+        return nombre;
     }
 }
